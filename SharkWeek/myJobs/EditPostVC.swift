c21e680c8d2d29bebbb51dc45c5f38273d305b9a
Abdi <abdikadirpro@gmail.com>
2018-10-16 17:15:45 -0600

    fixed the views and hooked them up to their view controllers

---
+++ SharkWeek/myJobs/EditPostVC.swift
@@ -0,0 +1,56 @@
+//
+//  EditPostVC.swift
+//  SharkWeek
+//
+//  Created by Abdikadir Abdalla on 10/16/18.
+//  Copyright © 2018 Cody Adcock. All rights reserved.
+//
+
+import UIKit
+
+class EditPostVC: UIViewController {
+    
+    @IBOutlet weak var jobTitleTF: UITextField!
+    @IBOutlet weak var payTF: UITextField!
+    @IBOutlet weak var addressOneTF: UITextField!
+    @IBOutlet weak var addressTwoTF: UITextField!
+    @IBOutlet weak var cityTF: UITextField!
+    @IBOutlet weak var stateTF: UITextField!
+    @IBOutlet weak var zipCodeTF: UITextField!
+    @IBOutlet weak var descriptionTV: UITextView!
+    @IBOutlet weak var toolsNeededTF: UITextField!
+    @IBOutlet weak var toolsProvidedTF: UITextField!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func updateButton(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func cancelButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func selectCategoryButton(_ sender: Any) {
+        
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SharkWeek/myJobs/PostJobVC.swift
@@ -0,0 +1,52 @@
+//
+//  PostJobVC.swift
+//  SharkWeek
+//
+//  Created by Abdikadir Abdalla on 10/16/18.
+//  Copyright © 2018 Cody Adcock. All rights reserved.
+//
+
+import UIKit
+
+class PostJobVC: UIViewController {
+    
+        @IBOutlet weak var jobTitleTF: UITextField!
+        @IBOutlet weak var payTF: UITextField!
+        @IBOutlet weak var addressOneTF: UITextField!
+        @IBOutlet weak var addressTwoTF: UITextField!
+        @IBOutlet weak var cityTF: UITextField!
+        @IBOutlet weak var stateTF: UITextField!
+        @IBOutlet weak var zipCodeTF: UITextField!
+        @IBOutlet weak var descriptionTV: UITextView!
+        @IBOutlet weak var toolsNeededTF: UITextField!
+        @IBOutlet weak var toolsProvidedTF: UITextField!
+    
+        override func viewDidLoad() {
+            super.viewDidLoad()
+        }
+    
+    @IBAction func postJobButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func selectCategoryButton(_ sender: Any) {
+            
+            
+    }
+        
+        
+        
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
+    }
+
+
+
