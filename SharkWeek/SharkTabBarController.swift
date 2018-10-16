0191c0af5ae0aeb0c84ca1dd6a2b2da7369cdb76
Cody <codyadcock10@gmail.com>
2018-10-16 14:13:37 -0600

    Added all the tabs with proper label names

--- SharkWeek/SharkTabBarController.swift
+++ SharkWeek/SharkTabBarController.swift
@@ -13,17 +13,19 @@ class SharkTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let profileNav = UIStoryboard(name: "Profile", bundle: .main).instantiateInitialViewController(),
-            let homeNav = UIStoryboard(name: "Home", bundle: .main).instantiateInitialViewController(),
+        guard let profileNav = UIStoryboard(name: "Home", bundle: .main).instantiateInitialViewController(),
+            let homeNav = UIStoryboard(name: "Profile", bundle: .main).instantiateInitialViewController(),
             let postJobNav = UIStoryboard(name: "PostJob", bundle: .main).instantiateInitialViewController(),
-            let myJobsNav = UIStoryboard(name: "MyJobs", bundle: .main).instantiateInitialViewController() else {return}
+            let myJobsNav = UIStoryboard(name: "MyJobs", bundle: .main).instantiateInitialViewController(),
+        let searchNav = UIStoryboard(name: "Search", bundle: .main).instantiateInitialViewController()else {return}
         
-        profileNav.tabBarItem = UITabBarItem(tabBarSystemItem: .contacts, tag: 0)
-        homeNav.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
-        postJobNav.tabBarItem = UITabBarItem(tabBarSystemItem: .more, tag: 2)
-        myJobsNav.tabBarItem = UITabBarItem(tabBarSystemItem: .recents, tag: 3)
+        homeNav.tabBarItem = UITabBarItem(title: "Home", image: nil, tag: 0)
+        profileNav.tabBarItem = UITabBarItem(title: "Profile", image: nil, tag: 1)
+        postJobNav.tabBarItem = UITabBarItem(title: "Post Job", image: nil, tag: 2)
+        myJobsNav.tabBarItem = UITabBarItem(title: "My Jobs", image: nil, tag: 3)
+        searchNav.tabBarItem = UITabBarItem(title: "Search", image: nil, tag: 4)
         
-        self.viewControllers = [profileNav, homeNav, postJobNav, myJobsNav]
+        self.viewControllers = [profileNav, homeNav, postJobNav, myJobsNav,searchNav]
     }
     
     
