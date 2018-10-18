caaed3a40aeb27a681f9d4517b5cb80e5f4f9549
Abdi <abdikadirpro@gmail.com>
2018-10-18 16:36:54 -0600

    did the segmented control for view job

--- SharkWeek/View Controller/PostedDetailVC.swift
+++ SharkWeek/View Controller/PostedDetailVC.swift
@@ -14,22 +14,43 @@ class PostedDetailVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        segmentAttributes()
     }
+    
+    func segmentAttributes() {
+        segmentedControlLabel.layer.cornerRadius = 5.0
+        segmentedControlLabel.backgroundColor = .lightGray
+        segmentedControlLabel.tintColor = .darkGray
+        
+        segmentedControlLabel.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: UIControl.State.selected)
+    }
+    
+    var detailArray: User?
     
     @IBAction func segmentedControlAction(_ sender: Any) {
+        switch segmentedControlLabel.selectedSegmentIndex {
+        case 0:
+            performSegue(withIdentifier: "segueToViewPosting", sender: self)
+            loadViewIfNeeded()
+        case 1:
+            performSegue(withIdentifier: "segueToEditPost", sender: self)
+            loadViewIfNeeded()
+        case 2:
+            performSegue(withIdentifier: "segueToViewApplicants", sender: self)
+            loadViewIfNeeded()
+        default: break
+        }
         
         
+        /*
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+         */
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
