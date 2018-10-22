4fca5290fab971ac0e070a54aafdfe89e63fdda1
Cody <codyadcock10@gmail.com>
2018-10-22 15:22:28 -0600

    rating pop up added

---
+++ SharkWeek/View Controller/RatingViewController.swift
@@ -0,0 +1,89 @@
+//
+//  RatingViewController.swift
+//  SharkWeek
+//
+//  Created by Cody on 10/22/18.
+//  Copyright © 2018 Cody Adcock. All rights reserved.
+//
+
+import UIKit
+
+class RatingViewController: UIViewController {
+    
+    var rating: Int?
+    
+    @IBOutlet weak var whiteBackView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var firstStar: UIButton!
+    @IBOutlet weak var secondStar: UIButton!
+    @IBOutlet weak var thirdStar: UIButton!
+    @IBOutlet weak var fourthStar: UIButton!
+    @IBOutlet weak var fifthStar: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        descriptionTextView.layer.borderWidth = 0.5
+        descriptionTextView.layer.borderColor = #colorLiteral(red: 0.643494308, green: 0.6439372897, blue: 0.6583478451, alpha: 1)
+        descriptionTextView.layer.cornerRadius = 5
+        
+        whiteBackView.layer.borderWidth = 0.5
+        whiteBackView.layer.borderColor = #colorLiteral(red: 0.643494308, green: 0.6439372897, blue: 0.6583478451, alpha: 1)
+        whiteBackView.layer.cornerRadius = 5
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+        
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        guard let flavorText = descriptionTextView.text else {return}
+        if flavorText == "" {return}
+        guard let rating = rating else {return}
+        print("\(rating) star rating! Description: \(flavorText) ")
+        self.dismiss(animated: true) {}
+    }
+    @IBAction func oneStarButtonTapped(_ sender: Any) {
+        firstStar.setTitle("★", for: .normal)
+        secondStar.setTitle("☆", for: .normal)
+        thirdStar.setTitle("☆", for: .normal)
+        fourthStar.setTitle("☆", for: .normal)
+        fifthStar.setTitle("☆", for: .normal)
+        rating = 1
+    }
+    @IBAction func twoStarButtonTapped(_ sender: Any) {
+        firstStar.setTitle("★", for: .normal)
+        secondStar.setTitle("★", for: .normal)
+        thirdStar.setTitle("☆", for: .normal)
+        fourthStar.setTitle("☆", for: .normal)
+        fifthStar.setTitle("☆", for: .normal)
+        rating = 2
+    }
+    @IBAction func threeStarButtonTapped(_ sender: Any) {
+        firstStar.setTitle("★", for: .normal)
+        secondStar.setTitle("★", for: .normal)
+        thirdStar.setTitle("★", for: .normal)
+        fourthStar.setTitle("☆", for: .normal)
+        fifthStar.setTitle("☆", for: .normal)
+        rating = 3
+    }
+    @IBAction func fourStarButtonTapped(_ sender: Any) {
+        firstStar.setTitle("★", for: .normal)
+        secondStar.setTitle("★", for: .normal)
+        thirdStar.setTitle("★", for: .normal)
+        fourthStar.setTitle("★", for: .normal)
+        fifthStar.setTitle("☆", for: .normal)
+        rating = 4
+    }
+    @IBAction func fiveStarButtonTapped(_ sender: Any) {
+        firstStar.setTitle("★", for: .normal)
+        secondStar.setTitle("★", for: .normal)
+        thirdStar.setTitle("★", for: .normal)
+        fourthStar.setTitle("★", for: .normal)
+        fifthStar.setTitle("★", for: .normal)
+        rating = 5
+    }
+    
+}
